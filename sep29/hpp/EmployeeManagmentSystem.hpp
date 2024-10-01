409cbfecb8022808137f79874017b6f5425ad9b6
tigranyeghyan <tigran.yeghyan.y.2004@gmail.com>
2024-10-01 23:56:48 +0400

    Employee Managment System

--- sep29/hpp/EmployeeManagmentSystem.hpp
+++ sep29/hpp/EmployeeManagmentSystem.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-class EmployeeManagmentSystem
+class EmployeeManagmentSystem : public ISalaryCal
 {
 	void addEmployee(shared_ptr<Employee> employee);
-	void displayAllEmployeesByDepartment(Type department) const;
-	void calculateTotalSalaryByDepartment(Type department) const;
-	removeEmployeeFromDepartment(const int id, Type);
-
+	void displayAllEmployeesByDepartment(DepartmentType department) const;
+	void calculateTotalSalaryByDepartment(DepartmentType department) const;
+	void removeEmployeeFromDepartment(const int id);
+	double calculateSalary(shared_ptr<Employee> empl) override;
 private:
 	shared_ptr<FinanceManager> finance_manager_;
 	shared_ptr<HRManager> hr_manager_;
